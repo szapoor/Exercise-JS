70840abcba4011e87caa829bde3638448a0a81bb
szapoor <szapoor@users.noreply.github.com>
2020-06-09 14:29:18 +0200

    Exercises-js

---
+++ scripts.js
@@ -0,0 +1,97 @@
+let resultOfAge= 2020-1999;
+
+undefined
+resultOfAge
+21
+let resultOfWw2= 1945-1939;
+undefined
+resultOfWw2
+6
+let resultOfTesla2= 76*920;
+undefined
+resultOfTesla2
+69920
+let resultSecondsOfDay= 24*60*60;
+undefined
+resultSecondsOfDay
+86400
+let resultOfHungarians= 7759660200 % 9772756;
+undefined
+resultOfHungarians
+91936
+let resultOfBigger= 9597000-78866;
+undefined
+resultOfBigger
+9518134
+let names= ['Lamar', 'Jay', 'Anna' ]
+undefined
+console.log(names.length)
+VM886:1 3
+undefined
+let third= names(names.length -1]
+VM958:1 Uncaught SyntaxError: missing ) after argument list
+let third= names[names.length -1]
+undefined
+console.log(third.length)
+1 4
+undefined
+let newLength= names.push('Chris')
+undefined
+console.log(names.push)
+VM1279:1 ƒ push() { [native code] }
+undefined
+let first= names.shift()
+undefined
+console.log(names.length)
+VM1372:1 3
+undefined
+let first= names.shift('Lamar')
+undefined
+console.log(names.length)
+VM1429:1 2
+undefined
+let first=names.push('Sandra')
+undefined
+console.log(names.length)
+VM1525:1 3
+undefined
+if (count($arr) => 4)
+1 
+console.log(names.length)
+ 3
+undefined
+
+let colors= {};
+undefined
+let myArray= ['black','white','grey']
+    
+undefined
+
+let myObject2= {
+    name: "colors",
+};
+undefined
+let myObject2= {
+    name:"colors",
+    arrayOne: ['black', 'white', 'yellow'],
+};
+undefined
+let myObject2= {
+    name:"colors",
+    arrayOne: ['black', 'white', 'yellow'],
+};
+undefined
+let myObject2= {
+    name:"colors",
+    arrayOne: ['black', 'white', 'yellow'],
+    name2:"hasManyColors",
+};
+undefined
+
+if (myObject2.length > 3){
+    console.log ("true");
+} else {
+    console.log ("false");
+}
+false
+undefined
